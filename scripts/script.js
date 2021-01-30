e3772a5a1f9afe6cfad1bb3b31175bcb905ff07f
Johnshandsable <Jshands@asu.edu>
2021-01-30 14:51:24 -0600

    added extra functionality

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   [x] A 'Submit' button should collect the form information, 
   [x] store the information to calculate monthly costs, 
   [x] append information to the DOM and clear the input fields. 
-  [] Using the stored information, calculate monthly costs 
+  [x] Using the stored information, calculate monthly costs 
   [] and append this to the to DOM. If the total monthly cost exceeds $20,000, 
   [] add a red background color to the total monthly cost.
   */
@@ -15,7 +15,6 @@ $(document).ready(function () {
   $('#submitEmployeeForm').on('click', submitForm); // event handler for submitButton
 });
 
-let monthlyCosts = 0;
 let employees = [];
 
 function submitForm() {
@@ -27,25 +26,32 @@ function collectInformation() {
   // collects information from user inputs
   let annualSalary = Number($('#annualSalary').val());
   let idNumber = Number($('#idNumber').val());
+
   const employee = {
     firstName: $('#firstName').val(),
     lastName: $('#lastName').val(),
     idNumber: idNumber,
     jobTitle: $('#jobTitle').val(),
     annualSalary: annualSalary,
+    monthlySalary: getMonthlySalary(annualSalary),
   };
-  // console.log(employee);
   employees.push(employee);
   renderEmployees();
 } // end collectInformation
 
-function addToMonthlyCosts(salary) {
-  // adds up monthly costs
+function getMonthlySalary(salary) {
   let employeeMonthly = Math.round(salary / 12);
-  console.log(employeeMonthly);
-  monthlyCosts += employeeMonthly; // salary represent annual income here
   return employeeMonthly; // return only the individual monthly income
-} // end addToMonthlyCosts
+} // end getMonthlySalary
+
+function getMonthlyCosts() {
+  console.log('getting monthly costs');
+  let sum = 0;
+  for (let employee of employees) {
+    sum += employee.monthlySalary;
+  }
+  return sum;
+}
 
 function renderEmployees() {
   console.log('render employees');
@@ -53,9 +59,10 @@ function renderEmployees() {
   $employeeInfo.empty(); // emptying out previous employee info so it doesn't get added on
   let $monthlyCost = $('#totalMonthly');
   $monthlyCost.empty();
-
+  let monthlyCosts = getMonthlyCosts();
   for (let employee of employees) {
     // Possibly add something on the table for users to delete people
+    // TODO: take out function from employeeInfo
     $employeeInfo.append(`
       <tr>
         <td>${employee.firstName}</td>
@@ -63,7 +70,7 @@ function renderEmployees() {
         <td>${employee.idNumber}</td>
         <td>${employee.jobTitle}</td>
         <td>$ ${employee.annualSalary}</td>
-        <td>$ ${addToMonthlyCosts(employee.annualSalary)}</td>
+        <td>$ ${employee.monthlySalary}</td>
       </tr>
   `);
   }
@@ -79,4 +86,9 @@ function renderEmployees() {
 
 function clearInputs() {
   console.log('clearing inputs');
-}
+  $('#firstName').val('');
+  $('#lastName').val('');
+  $('#idNumber').val('');
+  $('#jobTitle').val('');
+  $('#annualSalary').val('');
+} // end clearInputs
